40a95f7886278a36eddfe7f82f15633de7529bde
Anderson Freire Flores <andersonfflores75@gmail.com>
2023-10-28 23:00:51 -0300

    3.0. Nesta versão o arrayList retornou.

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,7 +1,10 @@
 package Application;
 
 import Entities.Fila;
+import ParteGrafica.MyFrame;
+import ParteGrafica.MyLabel;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
@@ -13,12 +16,17 @@ public class Main {
         int tamanho = Integer.parseInt(sc.nextLine());
         System.out.println("Insira o num de passadas até a batata explodir: ");
         int batataTimer = Integer.parseInt(sc.nextLine());
-        Fila fila = new Fila(tamanho);
+        Fila fila = new Fila();
+
         for (int i = 0; i < tamanho; i++) {
             System.out.println("Digite um nome para ser adicionado: ");
-            fila.enfileirar(sc.nextLine());
+            String nome = sc.nextLine();
+            fila.adicionarJogador(nome);
+
         }
-        fila.jogo(batataTimer);
+
+        String vencedor = fila.jogo(batataTimer);
+        System.out.println("O vencedor é: " + vencedor);
 
     }
 }
--- src/Entities/Fila.java
+++ src/Entities/Fila.java
@@ -1,70 +1,45 @@
 package Entities;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Fila {
-    private int tamanhoMaximo;
-    private String[] fila ;
-    private int frente;
-    private int traseira;
-    private int tamanhoAtual;
 
-    public Fila(int tamanhoMaximo) {
-        this.tamanhoMaximo = tamanhoMaximo;
-        this.fila = new String[tamanhoMaximo];
-        this.frente = 0;
-        this.traseira = -1;
-        this.tamanhoAtual = 0;
+
+    private ArrayList<String> jogadores = new ArrayList<>();
+
+    public void adicionarJogador(String jogador) {
+        jogadores.add(jogador);
     }
 
-    public boolean estaVazia() {
-        return tamanhoAtual == 0;
-    }
-
-    public boolean estaCheia() {
-        return tamanhoAtual == tamanhoMaximo;
-    }
-
-    public void enfileirar(String elemento) {
-        if (estaCheia()) {
-            System.out.println("Fila está cheia. Não é possível enfileirar mais elementos.");
-        } else {
-            traseira = (traseira + 1) % tamanhoMaximo;
-            fila[traseira] = elemento;
-            tamanhoAtual++;
+    public String moverJogador() {
+        String jogador = jogadores.remove(0);
+        if (jogadores.isEmpty()){
+            jogadores.add(0,jogador);
+        }else {
+            jogadores.add(jogadores.size(),jogador);
         }
+        return jogador;
     }
 
-    public String desenfileirar() {
-        if (estaVazia()) {
-            System.out.println("Fila está vazia. Não é possível desenfileirar elementos.");
-            return null; // Valor de retorno inválido se a fila estiver vazia
-        } else {
-
-            String elementoDesenfileirado = fila[frente];
-            frente = (frente + 1) % tamanhoMaximo;
-            tamanhoAtual--;
-            return elementoDesenfileirado;
-        }
-    }
-    public void jogo (int batataTimer){
-        //for para repetição do jogo
-        for (int i = 0; i < tamanhoMaximo; i++) {
-            //for do print de quem esta com a batata
-            for (int j = 0; j < batataTimer; j++) {
-                System.out.println(Arrays.toString(fila));
-                System.out.println(frente + " esta com a batata!");
-            }
-            //eliminação|vitoria
-            if (tamanhoAtual > 1) {
-                String eliminado = desenfileirar();
-                System.out.println(eliminado + " esta eliminado!");
-            }
-            else {
-                String eliminado = desenfileirar();
-                System.out.println(eliminado + " é o vencedor!");
-            }
-        }
+    public String jogo(int batataTimer) {
+       int contador = 0;
+       while (contador < jogadores.size()+(batataTimer)) {
+           for (int i = 0; i < batataTimer - 1; i++) {
+               String jogadorAtual = moverJogador();
+               System.out.println(jogadorAtual + " está com a batata quente!");
+           }
+           String jogadormovido = moverJogador();
+           if (jogadores.size() == 1){
+               return jogadores.get(0);
+           }
+           System.out.println("A batata estorou na mão do jogador: "+jogadormovido);
+           System.out.println(jogadormovido + " foi removido da roda.");
+           jogadores.remove(jogadormovido);
+           contador ++;
+       }
+        return jogadores.get(0);
     }
 }
 
